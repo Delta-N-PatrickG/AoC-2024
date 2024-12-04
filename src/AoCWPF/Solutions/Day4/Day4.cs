fcbe34f84382cca4d59c962133d09ab34bfc9ba3
Patrick Geelen <patrickg@delta-n.nl>
2024-12-04 12:33:30 +0100

    added documentation

--- src/AoCWPF/Solutions/Day4/Day4.cs
+++ src/AoCWPF/Solutions/Day4/Day4.cs
@@ -1,14 +1,10 @@
-﻿using Businesslogic.Locations;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using System.Diagnostics;
 
 namespace AoCWPF.Solutions
 {
+    /// <summary>
+    /// Represents the solution for Day 4 of the Advent of Code challenge.
+    /// </summary>
     public class Day4(int day, int part) : DayBase(day, part)
     {
         private int _day { get; set; } = day;
@@ -16,16 +12,20 @@ namespace AoCWPF.Solutions
         private List<List<string>> _grid;
 
         private int[][] _possibleDirections = {
-                [0, 1],  // Right
-                [1, 0],  // Down
-                [1, 1],  // Down-Right
-                [1, -1], // Down-Left
-                [0, -1], // Left
-                [-1, 0], // Up
-                [-1, 1], // Up-Right
-                [-1, -1] // Up-Left
-            };
+                    [0, 1],  // Right
+                    [1, 0],  // Down
+                    [1, 1],  // Down-Right
+                    [1, -1], // Down-Left
+                    [0, -1], // Left
+                    [-1, 0], // Up
+                    [-1, 1], // Up-Right
+                    [-1, -1] // Up-Left
+                };
 
+        /// <summary>
+        /// Solves part 1 of the challenge.
+        /// </summary>
+        /// <returns>The result of part 1 as a string.</returns>
         public override string Part1()
         {
             var search = "XMAS";
@@ -35,7 +35,10 @@ namespace AoCWPF.Solutions
             return result.ToString();
         }
 
-
+        /// <summary>
+        /// Solves part 2 of the challenge.
+        /// </summary>
+        /// <returns>The result of part 2 as a string.</returns>
         public override string Part2()
         {
             _grid = GetGrid();
@@ -44,6 +47,10 @@ namespace AoCWPF.Solutions
             return result.ToString();
         }
 
+        /// <summary>
+        /// Parses the raw input into a grid of characters.
+        /// </summary>
+        /// <returns>A grid of characters as a list of lists of strings.</returns>
         private List<List<string>> GetGrid()
         {
             return RawInput
@@ -52,6 +59,12 @@ namespace AoCWPF.Solutions
              .ToList();
         }
 
+        /// <summary>
+        /// Counts the number of times a specific word appears in the grid in any direction.
+        /// </summary>
+        /// <param name="grid">The grid of characters to search within.</param>
+        /// <param name="search">The word to search for in the grid.</param>
+        /// <returns>The count of the word found in the grid.</returns>
         private int CountAmountOfWord(List<List<string>> grid, string search)
         {
             var count = 0;
@@ -70,6 +83,14 @@ namespace AoCWPF.Solutions
             return count;
         }
 
+        /// <summary>
+        /// Checks all possible directions from a starting point in the grid for a specific word.
+        /// </summary>
+        /// <param name="grid">The grid of characters to search within.</param>
+        /// <param name="search">The word to search for in the grid.</param>
+        /// <param name="row">The starting row index.</param>
+        /// <param name="col">The starting column index.</param>
+        /// <returns>The count of the word found in all directions from the starting point.</returns>
         private int CheckAllDirections(List<List<string>> grid, string search, int row, int col)
         {
             var count = 0;
@@ -98,6 +119,11 @@ namespace AoCWPF.Solutions
             return count;
         }
 
+        /// <summary>
+        /// Counts the number of "MAS" crosses in the grid.
+        /// </summary>
+        /// <param name="grid">The grid of characters to search within.</param>
+        /// <returns>The count of "MAS" crosses found in the grid.</returns>
         private int MASCrossCount(List<List<string>> grid)
         {
             var count = 0;
@@ -116,6 +142,13 @@ namespace AoCWPF.Solutions
             return count;
         }
 
+        /// <summary>
+        /// Checks the surrounding positions for "M" and "S" characters to form a "MAS" cross.
+        /// </summary>
+        /// <param name="grid">The grid of characters to search within.</param>
+        /// <param name="r">The row index of the "A" character.</param>
+        /// <param name="c">The column index of the "A" character.</param>
+        /// <returns>The count of valid "MAS" crosses found.</returns>
         private int CheckDirections(List<List<string>> grid, int r, int c)
         {
             var count = 0;
@@ -123,10 +156,10 @@ namespace AoCWPF.Solutions
             // Define the positions to check for 'M' and 'S'
             var positions = new (int, int, string)[]
             {
-                (r - 1, c - 1, "M"), (r - 1, c + 1, "M"), (r + 1, c - 1, "S"), (r + 1, c + 1, "S"),
-                (r - 1, c - 1, "M"), (r + 1, c - 1, "M"), (r - 1, c + 1, "S"), (r + 1, c + 1, "S"),
-                (r + 1, c - 1, "M"), (r + 1, c + 1, "M"), (r - 1, c - 1, "S"), (r - 1, c + 1, "S"),
-                (r - 1, c + 1, "M"), (r + 1, c + 1, "M"), (r - 1, c - 1, "S"), (r + 1, c - 1, "S")
+                    (r - 1, c - 1, "M"), (r - 1, c + 1, "M"), (r + 1, c - 1, "S"), (r + 1, c + 1, "S"),
+                    (r - 1, c - 1, "M"), (r + 1, c - 1, "M"), (r - 1, c + 1, "S"), (r + 1, c + 1, "S"),
+                    (r + 1, c - 1, "M"), (r + 1, c + 1, "M"), (r - 1, c - 1, "S"), (r - 1, c + 1, "S"),
+                    (r - 1, c + 1, "M"), (r + 1, c + 1, "M"), (r - 1, c - 1, "S"), (r + 1, c - 1, "S")
             };
 
             // Check the positions in groups of four
@@ -144,8 +177,16 @@ namespace AoCWPF.Solutions
             return count;
         }
 
+        /// <summary>
+        /// Validates if a position in the grid contains the expected character.
+        /// </summary>
+        /// <param name="grid">The grid of characters to search within.</param>
+        /// <param name="r">The row index to check.</param>
+        /// <param name="c">The column index to check.</param>
+        /// <param name="expected">The expected character at the position.</param>
+        /// <returns>True if the position is valid and contains the expected character, otherwise false.</returns>
         private bool IsValid(List<List<string>> grid, int r, int c, string expected)
-        { 
+        {
             var isValid = r >= 0 && r < grid.Count && c >= 0 && c < grid[0].Count && grid[r][c] == expected;
             if (!isValid)
             {
